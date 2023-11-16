3d68af585abfe071cbe72ee67a90485278ff96b5
bramvd4 <135610086+bramvd4@users.noreply.github.com>
2023-11-16 12:09:18 +0100

    added click

--- src/just_count/count_count.py
+++ src/just_count/count_count.py
@@ -1,3 +1,20 @@
-import square
+import click
 
-print(f"The square of 5 is {square.square(5)}")
+from just_count import square
+
+
+@click.command()
+@click.option(
+    "-v",
+    "--value",
+    type=int,
+    default=0,
+    help="Value used for calculating the square",
+    show_default=True,
+)
+def main(value):
+    print(f"The square of {value} is {round(square.square(value))}")
+
+
+if __name__ == "__main__":
+    main()
